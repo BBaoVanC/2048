5c50507f0e64c5e8f5485ea2bf8c35e213218503
BBaoVanC <bbaovanc@bbaovanc.com>
2025-05-06 23:18:00 -0500

    Upload

--- src/display.cpp
+++ src/display.cpp
@@ -2,37 +2,65 @@
 #include <iostream>
 #include <string>
 
+#include <ncurses.h>
+
 #include "display.hpp"
 #include "game.hpp"
 
+#define MAX_COLOR 15
+
 Display::Display() {
-    std::cout << "Welcome to 2048!\n" << std::endl;
+    initscr();
+    keypad(stdscr, TRUE);
+    noecho();
+    curs_set(0);
+
+    start_color();
+    use_default_colors();
+
+    for (int x = 1; x <= MAX_COLOR; x++) {
+        // id, fg, bg
+        init_pair(x, x, -1);
+    }
 }
 
 Display::~Display() {
-    std::cout << "\n\n" << "GAME OVER" << std::endl;
+    endwin();
 }
 
 static void print_horizontal_row_separator() {
     // 4 cols, each 7 chars wide, plus 5 separator bars
-    std::cout << std::string(BOARD_SIZE * 7 + 5, '-') << "\n";
+    std::string p = std::string(BOARD_SIZE * 7 + 5, '-');
+    printw("%s", p.c_str());
 }
 static void print_empty_row() {
     for (int i = 0; i < BOARD_SIZE; i++) {
-        std::cout << '|' << std::string(7, ' ');
+        std::string p = "|" + std::string(7, ' ');
+        printw(p.c_str());
     }
-    std::cout << '|' << std::endl;
+    printw("|");
 }
 void Display::update(Game& game) {
-    std::cout << "Score: " << game.score << "\n"
-        << "Moves: " << game.moves << "\n";
+    erase();
 
-    std::cout << "\n";
+    mvprintw(0, 0, "Score: %u", game.score);
+    mvprintw(1, 0, "Moves: %u", game.moves);
+
+    size_t cur_y = 3;
+    move(cur_y, 0);
 
     for (size_t y = 0; y < BOARD_SIZE; y++) {
+        cur_y++;
+        move(cur_y, 0);
         print_horizontal_row_separator();
+
+        cur_y++;
+        move(cur_y, 0);
         print_empty_row();
-        std::cout << "|";
+
+        cur_y++;
+        move(cur_y, 0);
+        printw("|");
         for (size_t x = 0; x < BOARD_SIZE; x++) {
             std::string tile_text;
             if (!game.board[x][y].has_tile) {
@@ -41,13 +69,26 @@ void Display::update(Game& game) {
                 tile_text = std::to_string(1 << game.board[x][y].exp);
             }
 
-            //std::cout << std::vformat(tile_format, std::make_wformat_args(tile_text)) << " |";
-            std::cout << std::format("{:^7}|", tile_text);
+            int color = game.board[x][y].exp % 15 + 1;
+            attron(COLOR_PAIR(color));
+            std::string p = std::format("{:^7}", tile_text);
+            printw("%s", p.c_str());
+            attroff(COLOR_PAIR(color));
+            printw("|");
         }
-        std::cout << "\n";
+
+        cur_y++;
+        move(cur_y, 0);
         print_empty_row();
     }
+
+    cur_y++;
+    move(cur_y, 0);
     print_horizontal_row_separator();
 
-    std::cout << std::flush;
+    if (game.status == Status::Ended) {
+        mvprintw(3, 0, "GAME OVER! Press r to play again.");
+    }
+
+    refresh();
 }
--- src/game.cpp
+++ src/game.cpp
@@ -10,7 +10,7 @@
 
 Tile::Tile(): has_tile(false), exp(0) {}
 Tile::Tile(uint32_t exp): has_tile(true), exp(exp) {}
-bool Tile::operator==(const Tile& other) {
+bool Tile::operator==(const Tile& other) const {
     if (this->has_tile != other.has_tile) {
         return false;
     }
@@ -48,7 +48,7 @@ void Game::spawn_tile() {
     }
 }
 
-Game::Game(): board(), score(0), moves(0) {
+Game::Game(): board(), score(0), moves(0), status(Status::Playing) {
     srand(time(NULL));
 
     // place the first two tiles
@@ -56,26 +56,217 @@ Game::Game(): board(), score(0), moves(0) {
     this->spawn_tile();
 }
 
+// get tiles based on current x/y reference frame for the axes
+static Tile *get_tile_at_orientation(Tile board[BOARD_SIZE][BOARD_SIZE], bool orientation, size_t j, size_t k) {
+    if (orientation) {
+        return &board[k][j];
+    } else {
+        return &board[j][k];
+    }
+}
+// FIXME: remove this include
+#include <iostream>
+// this description based on the perspective of shifting left (orientation = false; direction = false):
+//
+// first, merge any tiles that can be
+// - start at the right, and see if there is an equal tile to the left
+// - if there is, then delete add right to left, and delete right. otherwise, go left one tile and check again
+// - done if we reach the leftmost tile. should not run the check since there's nothing to the left of it
+//
+// then, shift everything to the edge
+//
+// repeat all of this for each row
+//
+// orientation: true = up/down; false = left/right
+// direction: true = down/right; false = up/left
+// RETURNS: -1 if a move was not made, otherwise the score to be added
+static int64_t shift_tiles(Tile board[BOARD_SIZE][BOARD_SIZE], bool orientation, bool direction) {
+    int64_t score_delta = -1;
+
+    // j is the axis to be compressing along; k is the cross axis (vertical if shifting horizontally)
+
+    // doesn't matter which direction we traverse the cross axis
+
+    for (size_t k = 0; k < BOARD_SIZE; k++) {
+        // too complex to write in a for loop definition
+        size_t j;
+        if (direction) {
+            j = 0;
+        } else {
+            j = BOARD_SIZE - 1;
+        }
+        while ((direction && j < BOARD_SIZE - 1) || (!direction && j > 0)) {
+            Tile *tile_current = get_tile_at_orientation(board, orientation, j, k);
+            Tile *tile_into;
+            if (direction) {
+                tile_into = get_tile_at_orientation(board, orientation, j + 1, k);
+            } else {
+                tile_into = get_tile_at_orientation(board, orientation, j - 1, k);
+            }
+            if (!tile_current->has_tile) {
+                // ignore it
+            } else if (!tile_into->has_tile) {
+                // move tile to empty space
+                *tile_into = *tile_current;
+                tile_current->has_tile = false;
+                // a move was made
+                if (score_delta == -1) { score_delta = 0; }
+            } else if (tile_current->exp == tile_into->exp) {
+                tile_into->exp++;
+                tile_current->has_tile = false;
+                // a move was made, and added score
+                if (score_delta == -1) { score_delta = 0; }
+                score_delta += 1 << tile_into->exp;
+            }
+
+            if (direction) {
+                j++;
+            } else {
+                j--;
+            }
+        }
+    }
+
+    return score_delta;
+
+    //
+    for (size_t k = 0; k < BOARD_SIZE; k++) {
+        // checking conditions will be too complex to write inside a for loop definition
+        ssize_t j;
+        if (!direction) {
+            j = 0;
+        } else {
+            j = BOARD_SIZE - 1;
+        }
+        while ((!direction && j < BOARD_SIZE - 1) || (direction && j > 0)) {
+            // the tile to merge into
+            Tile *tile_merge;
+            if (!direction) {
+                tile_merge = get_tile_at_orientation(board, orientation, j + 1, k);
+            } else {
+                tile_merge = get_tile_at_orientation(board, orientation, j - 1, k);
+            }
+            // the tile to delete from the merge
+            Tile *tile_delete = get_tile_at_orientation(board, orientation, j, k);
+            if (tile_merge->has_tile && *tile_merge == *tile_delete) {
+                board[0][0].has_tile = true;
+                board[0][0].exp = 5;
+                if (!direction) {
+                    tile_delete->exp++;
+                    tile_merge->has_tile = false;
+                } else {
+                    tile_merge->exp++;
+                    tile_delete->has_tile = false;
+                }
+                score_delta = 1 << tile_merge->exp;
+                break;
+            }
+
+            if (!direction) { j++; } else { j--; }
+        }
+
+        ssize_t first_j = -1;
+        if ( (!direction && !get_tile_at_orientation(board, orientation, 0, k)->has_tile) ||
+              (direction && !get_tile_at_orientation(board, orientation, BOARD_SIZE - 1, k)->has_tile) ) {
+            // find the first tile
+            ssize_t j;
+            if (direction) {
+                j = BOARD_SIZE - 1;
+            } else {
+                j = 0;
+            }
+            while ((direction && j >= 0) || (!direction && j < BOARD_SIZE)) {
+                if (get_tile_at_orientation(board, orientation, j, k)->has_tile) {
+                    first_j = j;
+                }
+                if (direction) { j--; } else { j++; }
+            }
+        }
+        std::cerr << k << ' ' << first_j;
+        std::cerr << "\n\n";
+        continue;
+
+        if (first_j > 0) {
+            if (score_delta == -1) {
+                // if we didn't combine anything but do need to shift over some stuff
+                score_delta = 0;
+            }
+
+            // we need to move everything left by first_j many tiles
+            // skip the rightmost tile because it can be cleared after the loop
+            size_t j;
+            if (direction) {
+                j = BOARD_SIZE - 1;
+            } else {
+                j = 0;
+            }
+            while ((direction && j > 0) || (!direction && j < BOARD_SIZE - 1)) {
+                if (direction) {
+                    *get_tile_at_orientation(board, orientation, j, k) = *get_tile_at_orientation(board, orientation, j - first_j, k);
+                } else {
+                    *get_tile_at_orientation(board, orientation, j, k) = *get_tile_at_orientation(board, orientation, j + first_j, k);
+                }
+                if (direction) { j--; } else { j++; }
+            }
+            // clear that last tile
+            if (direction) {
+                get_tile_at_orientation(board, orientation, 0, k)->has_tile = false;
+            } else {
+                get_tile_at_orientation(board, orientation, BOARD_SIZE - 1, k)->has_tile = false;
+            }
+        }
+    }
+
+    return score_delta;
+}
 void Game::move(Move move) {
     // whether to add to the moves counter
-    bool made_move = false;
+    int64_t score_delta;
     switch (move) {
-        // this description based on the perspective of shifting left:
-        //
-        // first, merge any tiles that can be
-        // - start at the right, and see if there is an equal tile to the left
-        // - if there is, then delete add right to left, and delete right. otherwise, go left one tile and check again
-        // - done if we reach the leftmost tile. should not run the check since there's nothing to the left of it
-        //
-        // then, shift everything to the edge
-        //
-        // repeat all of this for each row
         case Move::Left:
-            for (size_t y = 0; y < BOARD_SIZE; y++) {
-                for (size_t x = 3; x > 0; x--) {
-                    //if (
-                }
-            }
+            score_delta = shift_tiles(this->board, false, false);
+            break;
+        case Move::Right:
+            score_delta = shift_tiles(this->board, false, true);
+            break;
+        case Move::Up:
+            score_delta = shift_tiles(this->board, true, false);
+            break;
+        case Move::Down:
+            score_delta = shift_tiles(this->board, true, true);
             break;
     }
+
+    if (score_delta >= 0) {
+        this->score += score_delta;
+        this->spawn_tile();
+        this->moves++;
+    // TODO: add game over screen (not currently working)
+    } else if (false) {
+        // if there's no possible moves, the game is over
+        for (ssize_t y = 0; y < BOARD_SIZE; y++) {
+            for (ssize_t x = 0; x < BOARD_SIZE; x++) {
+                // possible moves can be either up/down or left/right
+                Tile *current = &this->board[x][y];
+                for (ssize_t diff = -1; diff <= 1; diff += 2) {
+                    if (x + diff < 0 || y + diff < 0 || x + diff >= BOARD_SIZE || y + diff >= BOARD_SIZE) {
+                        // ignore out of bounds tiles
+                        continue;
+                    }
+
+                    if (!this->board[x + diff][y].has_tile || !this->board[x][y + diff].has_tile) {
+                        // there's an empty tile available
+                        return;
+                    }
+                    if (this->board[x + diff][y] == *current || this->board[x][y + diff] == *current) {
+                        // there's a tile that can be combined
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    //// otherwise, no possible moves, the game is over.
+    //this->status = Status::Ended;
 }
